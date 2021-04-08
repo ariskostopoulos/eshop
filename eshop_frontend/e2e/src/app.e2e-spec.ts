88b9cefad68b36218e5d487f18f2fde3371f1b1f
Aris Kostopoulos <arisbkostopoulos@gmail.com>
2021-04-08 12:33:10 +0300

    start

--- eshop_frontend/e2e/src/app.e2e-spec.ts
+++ eshop_frontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('eshop app is running!');
+    expect(await page.getTitleText()).toEqual('eshop_frontend app is running!');
   });
 
   afterEach(async () => {
--- eshop_frontend/src/app/app.component.spec.ts
+++ eshop_frontend/src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'eshop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('eshop');
+    expect(app.title).toEqual('eshop_frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('eshop app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('eshop_frontend app is running!');
   });
 });
